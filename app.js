ce3bcd559296283d6a094924f818bb3c6cb746c3
Ugin-gt <apach575@gmail.com>
2021-04-15 15:43:28 +0300

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const router = require('./routes');
 const errorHandler = require('./middlewares/error.handlers.js');
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
